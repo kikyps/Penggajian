0f878275be8516b0b56b8654fb1576a1a2f58457
kikyps <psrifki12@gmail.com>
2021-03-19 21:32:10 +0700

    signup2 back button

--- app/src/main/java/com/kp/penggajian/Common/LoginSignUp/SignupActivity2.java
+++ app/src/main/java/com/kp/penggajian/Common/LoginSignUp/SignupActivity2.java
@@ -2,7 +2,10 @@ package com.kp.penggajian.Common.LoginSignUp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.kp.penggajian.R;
 
@@ -13,7 +16,15 @@ public class SignupActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup2);
 
-
+        ImageView back = (ImageView) findViewById(R.id.signup_back_button);
+        back.setClickable(true);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
