9b3a86351b828b3eb9c6b734363df9423c8cf135
kikyps <psrifki12@gmail.com>
2021-04-07 08:24:48 +0700

    login firebase

--- app/src/main/java/com/kp/penggajian/Common/LoginSignUp/LoginActivity.java
+++ app/src/main/java/com/kp/penggajian/Common/LoginSignUp/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.kp.penggajian.Common.LoginSignUp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,13 +8,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.kp.penggajian.Dashboard;
+import com.kp.penggajian.MainActivity;
 import com.kp.penggajian.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextInputLayout usernameValid, passwordValid;
+    private TextInputLayout usernameValid, passwordValid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,13 +56,35 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!validateUsername() | !validatePassword()){
-                    return;
-                } else {
-                    Intent intent = new Intent(getApplicationContext(), SignupActivity2.class);
-                    startActivity(intent);
+                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+                databaseReference.child("login").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            String input1 = usernameValid.getEditText().getText().toString();
+                            String input2 = passwordValid.getEditText().getText().toString();
+
+                            if (!validateUsername() | !validatePassword()) {
+                                return;
+                            } else {
+                                if (snapshot.child(input1).exists()) {
+                                    if (snapshot.child(input1).child("password").getValue(String.class).equals(input2)) {
+                                        Intent intent = new Intent(getApplicationContext(), Dashboard.class);
+                                        startActivity(intent);
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), getString(R.string.wrong_password), Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    Toast.makeText(getApplicationContext(), getString(R.string.unknown_data), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
                 }
-            }
         });
     }
 
