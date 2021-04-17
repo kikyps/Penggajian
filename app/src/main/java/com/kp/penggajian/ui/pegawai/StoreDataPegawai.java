6aff8fa43a1343a3f37d9cc93e8e28766be42051
kikyps <psrifki12@gmail.com>
2021-04-17 22:37:06 +0700

    store data firebase

---
+++ app/src/main/java/com/kp/penggajian/ui/pegawai/StoreDataPegawai.java
@@ -0,0 +1,84 @@
+package com.kp.penggajian.ui.pegawai;
+
+public class StoreDataPegawai {
+
+    String sNik, sNamaPegawai, sGolongan, sTglLahir, sTglPensiun, sJabatan, sDivisi, sArea;
+
+    public StoreDataPegawai(){
+    }
+
+    public StoreDataPegawai(String sNik, String sNamaPegawai, String sGolongan, String sTglLahir, String sTglPensiun, String sJabatan, String sDivisi, String sArea) {
+        this.sNik = sNik;
+        this.sNamaPegawai = sNamaPegawai;
+        this.sGolongan = sGolongan;
+        this.sTglLahir = sTglLahir;
+        this.sTglPensiun = sTglPensiun;
+        this.sJabatan = sJabatan;
+        this.sDivisi = sDivisi;
+        this.sArea = sArea;
+    }
+
+    public String getsNik() {
+        return sNik;
+    }
+
+    public void setsNik(String sNik) {
+        this.sNik = sNik;
+    }
+
+    public String getsNamaPegawai() {
+        return sNamaPegawai;
+    }
+
+    public void setsNamaPegawai(String sNamaPegawai) {
+        this.sNamaPegawai = sNamaPegawai;
+    }
+
+    public String getsGolongan() {
+        return sGolongan;
+    }
+
+    public void setsGolongan(String sGolongan) {
+        this.sGolongan = sGolongan;
+    }
+
+    public String getsTglLahir() {
+        return sTglLahir;
+    }
+
+    public void setsTglLahir(String sTglLahir) {
+        this.sTglLahir = sTglLahir;
+    }
+
+    public String getsTglPensiun() {
+        return sTglPensiun;
+    }
+
+    public void setsTglPensiun(String sTglPensiun) {
+        this.sTglPensiun = sTglPensiun;
+    }
+
+    public String getsJabatan() {
+        return sJabatan;
+    }
+
+    public void setsJabatan(String sJabatan) {
+        this.sJabatan = sJabatan;
+    }
+
+    public String getsDivisi() {
+        return sDivisi;
+    }
+
+    public void setsDivisi(String sDivisi) {
+        this.sDivisi = sDivisi;
+    }
+
+    public String getsArea() {
+        return sArea;
+    }
+
+    public void setsArea(String sArea) {
+        this.sArea = sArea;
+    }
+}
--- app/src/main/java/com/kp/penggajian/ui/pegawai/TambahDataPegawai.java
+++ app/src/main/java/com/kp/penggajian/ui/pegawai/TambahDataPegawai.java
@@ -4,14 +4,24 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.kp.penggajian.R;
 
 import java.text.SimpleDateFormat;
@@ -22,7 +32,9 @@ import java.util.Locale;
 public class TambahDataPegawai extends AppCompatActivity {
 
     final Calendar myCalendar = Calendar.getInstance();
-    private TextInputEditText etTglLahir, etTglPensiun;
+    TextInputEditText etNik, etNamaPegawai, etGolongan, etTglLahir, etTglPensiun, etJabatan, etDevisi, etArea;
+    Button btnTambah;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +43,25 @@ public class TambahDataPegawai extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        btnTambah = findViewById(R.id.tambah_data);
+        etNik = findViewById(R.id.nik_pegawai);
+        etNamaPegawai = findViewById(R.id.nama_pegawai);
+        etGolongan = findViewById(R.id.golongan);
         etTglLahir = findViewById(R.id.tgl_lahir);
         etTglPensiun = findViewById(R.id.tgl_pensiun);
+        etJabatan = findViewById(R.id.jabatan);
+        etDevisi = findViewById(R.id.divisi);
+        etArea = findViewById(R.id.area);
+
+        etNik.addTextChangedListener(tambahData);
+        etNamaPegawai.addTextChangedListener(tambahData);
+        etGolongan.addTextChangedListener(tambahData);
+        etTglLahir.addTextChangedListener(tambahData);
+        etTglPensiun.addTextChangedListener(tambahData);
+        etJabatan.addTextChangedListener(tambahData);
+        etDevisi.addTextChangedListener(tambahData);
+        etArea.addTextChangedListener(tambahData);
+
         DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int monthOfYear,
@@ -78,12 +107,83 @@ public class TambahDataPegawai extends AppCompatActivity {
             }
         });
 
+        btnTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sNik = etNik.getText().toString().trim();
+                String sNamaPegawai = etNamaPegawai.getText().toString().trim();
+                String sGolongan = etGolongan.getText().toString().trim();
+                String sTglLahir = etTglLahir.getText().toString().trim();
+                String sTglPensiun = etTglPensiun.getText().toString().trim();
+                String sJabatan = etJabatan.getText().toString().trim();
+                String sDivisi = etDevisi.getText().toString().trim();
+                String sArea = etArea.getText().toString().trim();
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(TambahDataPegawai.this);
+                builder.setTitle(getResources().getString(R.string.add_data))
+                        .setMessage(getResources().getString(R.string.add_pegawai_message))
+                        .setPositiveButton(getResources().getString(R.string.yes_confirm), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                                StoreDataPegawai storeDataPegawai = new StoreDataPegawai(sNik, sNamaPegawai, sGolongan, sTglLahir, sTglPensiun, sJabatan, sDivisi, sArea);
+
+                                databaseReference.child("DataPegawai").push().setValue(storeDataPegawai).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Toast.makeText(getApplicationContext(), getResources().getString(R.string.add_pegawai_success), Toast.LENGTH_SHORT).show();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(getApplicationContext(), getResources().getString(R.string.failed_data_upload), Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        })
+                        .setNegativeButton(getResources().getString(R.string.no_confirm), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        });
+                builder.setCancelable(true);
+                builder.show();
+            }
+        });
+
         // add back arrow to toolbar
         if (getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
     }
+
+    private TextWatcher tambahData = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            String nik = etNik.getText().toString().trim();
+            String nama = etNamaPegawai.getText().toString().trim();
+            String golongan = etGolongan.getText().toString().trim();
+            String tgllahir = etTglLahir.getText().toString().trim();
+            String tglpensi = etTglPensiun.getText().toString().trim();
+            String jabatan = etJabatan.getText().toString().trim();
+            String divisi = etDevisi.getText().toString().trim();
+            String area = etArea.getText().toString().trim();
+
+            btnTambah.setEnabled(!nik.isEmpty() && !nama.isEmpty() && !golongan.isEmpty() && !tgllahir.isEmpty() && !tglpensi.isEmpty() && !jabatan.isEmpty() && !divisi.isEmpty() && !area.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
 
     private void updateTglLahir() {
         String myFormat = "dd/MM/yyyy"; //In which you need put here
@@ -95,6 +195,12 @@ public class TambahDataPegawai extends AppCompatActivity {
 
         etTglLahir.setText(sdf.format(today));
         etTglPensiun.setText(sdf.format(expiredDate));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        btnTambah.setEnabled(false);
     }
 
     private void updateTglPensiun() {
