2f153886952e37f1ace46ba042566e36ec635aff
kikyps <psrifki12@gmail.com>
2021-04-26 08:08:46 +0700

    semi-final

--- app/src/main/java/com/kp/penggajian/ui/dashboard/DashbordFragment.java
+++ app/src/main/java/com/kp/penggajian/ui/dashboard/DashbordFragment.java
@@ -1,5 +1,6 @@
 package com.kp.penggajian.ui.dashboard;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,24 +13,54 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.kp.penggajian.R;
 
 public class DashbordFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
+    TextView dataCount;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    ProgressDialog progressDialog;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        dataCount = root.findViewById(R.id.jumlah_pegawai);
+        customProgresBar();
+        getsCount();
+        return root;
+    }
+
+    private void getsCount(){
+        databaseReference.child("DataPegawai").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    int count = (int) snapshot.getChildrenCount();
+                    dataCount.setText(Integer.toString(count));
+                    progressDialog.dismiss();
+                } else {
+                    dataCount.setText("0");
+                    progressDialog.dismiss();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
-        return root;
+    }
+
+    public void customProgresBar(){
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+        progressDialog.setContentView(R.layout.cutom_progress_bar);
+        progressDialog.getWindow().setBackgroundDrawableResource(R.color.transparent);
     }
 }
